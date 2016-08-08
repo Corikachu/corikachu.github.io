2dfa3b7f2b50e31c1d886bb5f8da378c5dfb7e20
Corikachu <konomama0505@gmail.com>
2016-08-09 03:32:11 +0900

    open a new tab when click a url contained other domain.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-$(document).ready(function() {
-});
+// open a new tab when click a url contained other domain.
+$(document.links).filter(function() {
+    return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
